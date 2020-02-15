7b4691230a2c5673744ae9d286dd12e849919585
averholantsev <averholantsev@ics.perm.ru>
2020-02-15 22:33:50 +0500

    Updates

---
+++ precache-manifest.8004488b473590a975ea10e100ef7623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f567382d66880607a801faec244da3fd",
+    "url": "/auth-app/index.html"
+  },
+  {
+    "revision": "f72b44ac986822cd68c9",
+    "url": "/auth-app/static/css/main.bc2643f6.chunk.css"
+  },
+  {
+    "revision": "d195f9d38b91b98d6a97",
+    "url": "/auth-app/static/js/2.6c41ac56.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/auth-app/static/js/2.6c41ac56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72b44ac986822cd68c9",
+    "url": "/auth-app/static/js/main.ada072a2.chunk.js"
+  },
+  {
+    "revision": "fc4a5eeca769cf1dbb63",
+    "url": "/auth-app/static/js/runtime-main.3ec2ff6f.js"
+  }
+]);
